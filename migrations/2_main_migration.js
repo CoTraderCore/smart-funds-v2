bbc314c872ca2f9c1b92d4faf042baef98011edb
Ruslan <ruslan777mirov@gmail.com>
2019-12-13 16:17:55 +0200

    Update migration

--- migrations/2_main_migration.js
+++ migrations/2_main_migration.js
@@ -3,16 +3,16 @@ const ParaswapParams = artifacts.require('./ParaswapParams.sol')
 const SmartFundRegistry = artifacts.require('./SmartFundRegistry.sol')
 const ExchangePortal = artifacts.require('./ExchangePortal.sol')
 const PermittedExchanges = artifacts.require('./PermittedExchanges.sol')
-const PARASWAP_NETWORK_ADDRESS = '0x818E6FECD516Ecc3849DAf6845e3EC868087B755'
-const PARASWAP_PRICE_ADDRESS = "0x0e936B11c2e7b601055e58c7E32417187aF4de4a"
+const PARASWAP_NETWORK_ADDRESS = "0x72338b82800400f5488eca2b5a37270ba3b7a111"
+const PARASWAP_PRICE_ADDRESS = ""
 
 const PLATFORM_FEE = 1000
 
 module.exports = (deployer, network, accounts) => {
   deployer
-    .then(() => deployer.deploy(ExchangePortal, PARASWAP_NETWORK_ADDRESS, PARASWAP_PRICE_ADDRESS))
-    .then(() => deployer.deploy(PermittedExchanges, ExchangePortal.address))
     .then(() => deployer.deploy(ParaswapParams))
+    .then(() => deployer.deploy(ExchangePortal, PARASWAP_NETWORK_ADDRESS, PARASWAP_PRICE_ADDRESS, ParaswapParams.address))
+    .then(() => deployer.deploy(PermittedExchanges, ExchangePortal.address))
     .then(() =>
       deployer.deploy(
         SmartFundRegistry,
