77bf63c7b5e330c16e7345e2c773d60abffb5b5a
Ruslan <ruslan777mirov@gmail.com>
2019-12-27 11:39:53 +0200

    Remove date from permitted interface

--- contracts/PermittedExchangesInterface.sol
+++ contracts/PermittedExchangesInterface.sol
@@ -1,8 +1,5 @@
 pragma solidity ^0.4.23;
 
 contract PermittedExchangesInterface {
-
-  mapping (address => uint256) public exchangePermittedDate;
-
   mapping (address => bool) public permittedAddresses;
 }
